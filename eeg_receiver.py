98a774b5fbf52479c800a71b306b5979023a7ce9
Jonathan <jonathan.p1437@gmail.com>
2025-11-19 14:50:48 -0500

    Creating a way to get the bandpower from the eeg data

--- eeg_receiver.py
+++ eeg_receiver.py
@@ -1,11 +1,22 @@
 from pylsl import StreamInlet, resolve_byprop
 import numpy as np
+from scipy.signal import welch
+from scipy.integrate import simpson
 
-class Band:
-    Delta = 0
-    Theta = 1
-    Alpha = 2
-    Beta = 3
+bands = {
+    "delta": [0.5, 4],
+    "theta": [4, 8],
+    "alpha": [8, 12],
+    "beta": [12, 30]
+}
+
+TP9Buffer = [] #LT
+AF7Buffer = [] #LF
+AF8Buffer = [] #RF
+TP10Buffer = [] #RT
+AUXBuffer = [] #X
+ppgBufferRaw = [0, 0, 0]
+
 
 def create_and_resolve_inlet(name):
     """Create a new inlet to receive data from based on the specified type
@@ -20,31 +31,90 @@ def create_and_resolve_inlet(name):
     inlet = StreamInlet(streams[0])
     return inlet
 
-def receive_LSL_signal(eeg_inlet, ppg_inlet):
-    """ Create inlets and receive the data EEG & PPG from the MUSE headset"""
+def receive_LSL_signal():
+    """ 
+    Create inlets and receive the data EEG & PPG from the MUSE headset
+
+    This method will be run in the background as a separate thread
+    """
+    eeg_inlet = create_and_resolve_inlet('EEG')
+    ppg_inlet = create_and_resolve_inlet('PPG')
     eeg_sample = []
     ppg_sample = []
 
-    #Getting the rate of transfer of 
-    info = eeg_inlet.info()
-    fs = int(info.nominal_srate())
+    #Running a loop to
+    while True:
+        eeg_chunk, _ = eeg_inlet.pull_chunk(timeout=0.02, max_samples=32)
+        ppg_chunk, _ = ppg_inlet.pull_chunk(timeout=0.02, max_samples=8)
 
-    eeg_chunk, _ = eeg_inlet.pull_chunk(timeout=0.2, max_samples=32)
-    ppg_chunk, _ = ppg_inlet.pull_chunk(timeout=0.2, max_samples=8)
+        if eeg_chunk:
+            valid_rows = [row for row in eeg_chunk if 0.0 not in row]
+            if valid_rows:
+                eeg_sample = valid_rows[-1]
+                process_eeg_data(eeg_sample)
 
-    if eeg_chunk:
-        valid_rows = [row for row in eeg_chunk if 0.0 not in row]
-        if valid_rows:
-            eeg_sample = valid_rows[-1]
+        if ppg_chunk:
+            valid_rows = [row for row in ppg_chunk if 0.0 not in row]
+            if valid_rows:
+                ppg_sample = valid_rows[-1]
+                process_ppg_data(ppg_sample)
 
-    if ppg_chunk:
-        valid_rows = [row for row in ppg_chunk if 0.0 not in row]
-        if valid_rows:
-            ppg_sample = valid_rows[-1]
+def process_eeg_data(eeg_sample):
+    """
+    Processing the EEG data by putting it in the respective buffers
+
+    eeg_sample -- Contains an array with 5 values [TP9, AF7, AF8, TP10, AUX]
+    """
+    TP9Buffer.append(eeg_sample[0])
+    AF7Buffer.append(eeg_sample[1])
+    AF8Buffer.append(eeg_sample[2])
+    TP10Buffer.append(eeg_sample[3])
+    AUXBuffer.append(eeg_sample[4])
+
+def process_ppg_data(ppg_sample):
+    ppgBufferRaw.append(ppg_sample)
+
+
+def process_signal(buffer, win_sec):
+    """
+    Processing the signals to find the bandpower of each region averaged out
+
+    This means we will find delta, theta, alpha, and beta(no gamma) for the channels 
+    of my choice. For this I will just do AF7 and AF8 because they are the most 
+    reliable on a muse headset.
+    """  
+    values = [0, 0, 0, 0]
+    if len(buffer) == 512:
+        values[0] = round(bandpower(buffer, 256, bands['delta'], 2), 2)
+        values[1] = round(bandpower(buffer, 256, bands['theta'], 2), 2)
+        values[2] = round(bandpower(buffer, 256, bands['alpha'], 2), 2)
+        values[3] = round(bandpower(buffer, 256, bands['beta'], 2), 2)
+        del buffer[:22]
+    return values
+
+
+def bandpower(data, sf, band, window_sec=None, relative=False):
+    """Compute the average power of the signal x in a specific frequency band
     
-    return process_signals(eeg_sample, ppg_sample)
+    """
+    low, high = band
+    if window_sec is not None:
+        nperseg = window_sec * sf
+    else:
+        nperseg = (2 / low) * sf
 
-def process_signals(eeg_signals: list, ppg_signals: list):
-    """Processing the signals to determine the tilt factor of the player"""  
-    # Need to figure out how to get bandpower from the signals, do I need a buffer? well see
-        
+    #COmpute Welch
+    freqs, psd = welch(data, sf, nperseg=nperseg)
+
+    # Frequency resolution
+    freq_res = freqs[1] - freqs[0]
+
+    #Find closest indecies of band in freq vector
+    idx_band = np.logical_and(freqs >= low, freqs <= high)
+
+    #Integral approx of area
+    bp = simpson(psd[idx_band], dx=freq_res)
+
+    if relative:
+        bp /= simpson(psd, dx=freq_res)
+    return bp
