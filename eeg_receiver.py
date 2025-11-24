b4056a22a57695ee34ed918717ba97b643b0d990
Jonathan <jonathan.p1437@gmail.com>
2025-11-23 23:09:15 -0500

    Can find heart rate now

--- eeg_receiver.py
+++ eeg_receiver.py
@@ -16,6 +16,10 @@ class EEGReceiver:
         self.ip = ip
         self.port = port
         self.bands = {"delta": [0.5, 4], "theta": [4, 8], "alpha": [8, 12], "beta": [12, 30], "gamma": [30, 50]}
+        self.PPG_SAMPLE_RATE = 64
+        self.PPG_WINDOW_SIZE = 8 * self.PPG_SAMPLE_RATE
+        self.EEG_SAMPLE_RATE = 256
+        self.EEG_WINDOW_SIZE = 3 * self.EEG_SAMPLE_RATE
 
         #Synchronous values
         self.TP9Buffer = [] #LT
@@ -24,11 +28,13 @@ class EEGReceiver:
         self.TP10Buffer = [] #RT
         self.AUXBuffer = [] #X
         self.ppg_buffer = []
+
         self.latest_bandpower = []
 
         #Baseline values
         #WIP - Not sure the exact metrics to look at
         #Will include HRV and some bandpower thing for sure
+        self.baseline_metrics = None
 
         # Set up for the dispatcher 
         self.dispatcher = Dispatcher()
@@ -42,7 +48,7 @@ class EEGReceiver:
         thread = threading.Thread(target=self.server.serve_forever, daemon=True)
         thread.start()
     
-    def on_eeg(self, address, values*):
+    def on_eeg(self, address, *values):
         """
         Processing the EEG data by putting it in the respective buffers
 
@@ -54,12 +60,19 @@ class EEGReceiver:
         self.AF8Buffer.append(values[2])
         self.TP10Buffer.append(values[3])
         self.AUXBuffer.append(values[4])
+
+        if len(self.TP9Buffer) > self.EEG_WINDOW_SIZE:
+            del self.TP9Buffer[:self.EEG_SAMPLE_RATE]
+            del self.TP10Buffer[:self.EEG_SAMPLE_RATE]
+            del self.AF7Buffer[:self.EEG_SAMPLE_RATE]
+            del self.AF8Buffer[:self.EEG_SAMPLE_RATE]
+            del self.AUXBuffer[:self.EEG_SAMPLE_RATE]
     
-    def on_ppg(self, address, values*):
-        list = [0, 0, 0]
-        for idx, value in enumerate(values):
-            list[idx] = value
-        self.ppg_buffer.append(list)
+    def on_ppg(self, address, *values):
+        self.ppg_buffer.append(values[0]) #Only storing PPG1
+
+        if len(self.ppg_buffer) > self.PPG_WINDOW_SIZE:
+            del self.ppg_buffer[:self.PPG_SAMPLE_RATE]
 
     def process_signal(self, buffer, win_sec):
         """
@@ -114,8 +127,20 @@ class EEGReceiver:
             bp /= simpson(psd, dx=freq_res)
         return bp
 
-def compute_hrv():
-    """Getting the HRV from the PPG data"""
+    def compute_hrv(self):
+        """Getting the HRV from the PPG1 data"""
+        if len(self.ppg_buffer) < 7 * 64:
+            return None #Not enough data to get heart rate
+
+        data = self.ppg_buffer[-self.PPG_SAMPLE_RATE*7:]
+
+        try:
+            wd, m = hp.process(data, sample_rate=self.PPG_SAMPLE_RATE)
+            return m
+        except hp.exceptions.HeartPyError:
+            return None #Bad segment
+
+
 
 
 #Potential methods I could introduce later
